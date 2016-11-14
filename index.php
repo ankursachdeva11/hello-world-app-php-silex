738099f0a7e2ded144e3ff7f5450d0433696426b
Ankur Sachdeva <Ankur Sachdeva>
2016-11-14 16:16:05 +0530

    changes

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ setenv("BC_AUTH_SERVICE=https://login.bigcommerce.com");
 setenv("BC_CLIENT_ID=83uhqbudo5vowrxep8i9pu93i2j48xh");
 setenv("BC_CLIENT_SECRET=equ4hsyb1blmy19xcfgh0tuvps8hldf");
 
-//$app->get('/load', function (Request $request) use ($app) {
-if(isset($_REQUEST['method']) && $_REQUEST['method'] == 'load'){
-	$data = verifySignedRequest($_REQUEST['signed_payload']);
+$app->get('/load', function (Request $request) use ($app) {
+
+	$data = verifySignedRequest($request->get('signed_payload'));
 	if (empty($data)) {
 		return 'Invalid signed_payload.';
 	}
@@ -27,18 +27,18 @@ if(isset($_REQUEST['method']) && $_REQUEST['method'] == 'load'){
 		return 'Invalid user.';
 	}
 	return 'Welcome ' . json_encode($user);
-}//);
+});
 
-//$app->get('/auth/callback', function (Request $request) use ($app) {
-if(isset($_REQUEST['method']) && $_REQUEST['method'] == 'auth') {
+$app->get('/auth/callback', function (Request $request) use ($app) {
+
 	$payload = array(
 		'client_id' => clientId(),
 		'client_secret' => clientSecret(),
 		'redirect_uri' => callbackUrl(),
 		'grant_type' => 'authorization_code',
-		'code' => $_REQUEST['code'],
-		'scope' => $_REQUEST['scope'],
-		'context' => $_REQUEST['context'],
+		'code' => $request->get('code'),
+		'scope' => $request->get('scope'),
+		'context' => $request->get('context'),
 	);
 
 	$client = new Client(bcAuthService());
@@ -60,7 +60,7 @@ if(isset($_REQUEST['method']) && $_REQUEST['method'] == 'auth') {
 		return 'Something went wrong... [' . $resp->getStatusCode() . '] ' . $resp->getBody();
 	}
 
-}
+});
 
 function verifySignedRequest($signedRequest)
 {
